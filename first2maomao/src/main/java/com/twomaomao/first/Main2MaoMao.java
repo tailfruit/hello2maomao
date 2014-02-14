27fb3ec303d2cc1397e72f37bbd83f7887642c74
bo.li <bo.li@dianping.com>
2014-02-14 18:43:05 +0800

    新建项目。

---
+++ first2maomao/src/main/java/com/twomaomao/first/Main2MaoMao.java
@@ -0,0 +1,33 @@
+/**
+ * Project: first2maomao
+ * 
+ * File Created at 2014-2-14
+ * $Id$
+ * 
+ * Copyright 2010 dianping.com.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Dianping Company. ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with dianping.com.
+ */
+package com.twomaomao.first;
+
+/**
+ * TODO Comment of Main2MaoMao
+ * 
+ * @author bo.li
+ * 
+ */
+public class Main2MaoMao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello 2maomao, hello github :)");
+	}
+
+}
